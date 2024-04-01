6a8be8537dde484857fb9b7e3f3f3829ca53a97e
user <user@DESKTOP-DITHSK3>
2024-04-01 16:26:42 +0900

    commit

---
+++ 20240401/C/ASCIItest3.c
@@ -0,0 +1,14 @@
+////���� 119p ���� 5
+//#pragma warning(disable:6031)
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	char a;
+//
+//	printf("���ڸ� �Է��ϼ���: ");
+//	scanf("%c", &a);
+//
+//	printf("���� %c�� �ƽ�Ű�ڵ尪 : %d", a,a);
+//	return 0;
+//}
