d29f558c114e8a62662f84b0954092e717e33a34
user <user@DESKTOP-DITHSK3>
2024-04-01 14:43:00 +0900

    commit

---
+++ 20240401/ConvDiv.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int num1 = 3, num2 = 4;
+//	double divResult;
+//	divResult = (double)num1 / num2; //double 을 앞에 붙여도 뒤에 붙여도 같다
+//	printf("나눗셈 결과: %f \n", divResult);
+//	printf("앞집 강아지가 말했다. \"멍~!멍~?\" 정말 귀엽다");
+//	return 0;
+//}
+//
